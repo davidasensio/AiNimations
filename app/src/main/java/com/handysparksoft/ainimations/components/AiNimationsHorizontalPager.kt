263f59f77482eb2ef809111cc165f8045b303948
David Asensio <davidasensio81@gmail.com>
2023-05-12 12:16:09 +0200

    Add the PageIndicator to the HorizontalPager

--- app/src/main/java/com/handysparksoft/ainimations/components/AiNimationsHorizontalPager.kt
+++ app/src/main/java/com/handysparksoft/ainimations/components/AiNimationsHorizontalPager.kt
@@ -1,11 +1,14 @@
 package com.handysparksoft.ainimations.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -18,20 +21,29 @@ fun AiNimationsHorizontalPager(
     content: @Composable () -> Unit
 ) {
     val pagerState = rememberPagerState()
-    val items = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+    val pages = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
 
-    HorizontalPager(
-        pageCount = items.size,
-        pageSpacing = 16.dp,
-        beyondBoundsPageCount = 2,
-        state = pagerState,
-        modifier = modifier
-    ) { page ->
-        AiNimationsCard(
-            pagerState = pagerState,
-            page = page,
-            modifier = Modifier.padding(horizontal = 64.dp, vertical = 32.dp),
-            content = content
+    Box(modifier = modifier.fillMaxSize()) {
+        HorizontalPager(
+            pageCount = pages.size,
+            pageSpacing = 16.dp,
+            beyondBoundsPageCount = 2,
+            state = pagerState
+        ) { page ->
+            AiNimationsCard(
+                pagerState = pagerState,
+                page = page,
+                modifier = Modifier.padding(horizontal = 64.dp, vertical = 48.dp),
+                content = content
+            )
+        }
+
+        PageIndicator(
+            pageCount = pages.size,
+            selectedPage = pagerState.currentPage,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(12.dp)
         )
     }
 }
